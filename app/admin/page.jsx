078f1b05ae1af1a51e687055ed2ae8d0bb788535
faysal725 <afaysal725@gmail.com>
2025-03-21 15:15:54 +0600

    admin login created

---
+++ app/admin/page.jsx
@@ -0,0 +1,77 @@
+import { BoltIcon } from "@heroicons/react/24/outline";
+
+export default function AdminLoginPage() {
+  return (
+    <div className="flex min-h-screen flex-1 flex-col justify-center px-6 py-12 lg:px-8 bg-gray-900">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm ">
+        {/* <img
+          alt="Your Company"
+          src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
+          className="mx-auto h-10 w-auto"
+        /> */}
+
+        <div className="flex justify-center items-center gap-1">
+          <BoltIcon className="h-8 w-auto text-white" />
+          <p className="text-3xl font-bold text-white italic">Blazor</p>
+        </div>
+        <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white">
+          Admin Login
+        </h2>
+      </div>
+
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form action="#" method="POST" className="space-y-6">
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm/6 font-medium text-white"
+            >
+              Email address
+            </label>
+            <div className="mt-2">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                required
+                autoComplete="email"
+                className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="password"
+                className="block text-sm/6 font-medium text-white"
+              >
+                Password
+              </label>
+              
+            </div>
+            <div className="mt-2">
+              <input
+                id="password"
+                name="password"
+                type="password"
+                required
+                autoComplete="current-password"
+                className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+            >
+              Sign in
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
