cc860e0611310cb88c2c7a579f0c00ce5a1a3965
faysal725 <afaysal725@gmail.com>
2025-03-25 14:41:38 +0600

    cart page is building

--- app/product-details/[productId]/loading.jsx
+++ app/product-details/[productId]/loading.jsx
@@ -3,6 +3,9 @@ import React from "react";
 export default function loading() {
   return (
     <section className="mx-auto max-w-7xl  px-4 sm:px-6 lg:px-8 py-28 flex flex-col items-center justify-center">
+      
+
+      
       <p className="text-2xl font-semibold text-gray-600 animate-pulse">Loading Product ...</p>
     </section>
   );
